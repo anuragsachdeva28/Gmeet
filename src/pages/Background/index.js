b3534f8201fac60084bdd430484575c1440b0151
Anurag Sachdeva <anurag.sach28@gmail.com>
2020-10-18 01:21:43 +0530

    Complete Code

---
+++ src/pages/Background/index.js
@@ -0,0 +1,108 @@
+import '../../assets/img/icon-34.png';
+import '../../assets/img/icon-128.png';
+
+
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('Command:', command);
+  if (command === "copy-link") {
+    chrome.runtime.sendMessage({
+      msg: "copy_link"
+    });
+  }
+  else if (command === "create-meeting") {
+    chrome.runtime.sendMessage({
+      msg: "btn_press"
+    });
+    createMeeting();
+  }
+});
+
+function createMeeting() {
+  chrome.identity
+    .getAuthToken({ interactive: true }, function (token) {
+
+      console.log(token);
+
+      let startDate = new Date();
+      let endDate = new Date(startDate.getTime() + 3600000);
+      let isoStartDate = new Date(startDate.getTime()-new Date().getTimezoneOffset()*60*1000).toISOString().split(".")[0];
+      let isoEndDate = new Date(endDate.getTime()-(new Date().getTimezoneOffset())*60*1000).toISOString().split(".")[0];
+
+      let event = {
+        'summary': 'Create Google meet Meeting',
+        'description': 'Google Meeting created using a chrome extension GMeet',
+        'start': {
+          'dateTime': `${isoStartDate}`,
+          'timeZone': 'Asia/Kolkata'
+        },
+        'end': {
+          'dateTime': `${isoEndDate}`,
+          'timeZone': 'Asia/Kolkata'
+        },
+        'conferenceData': {
+          'createRequest': {'requestId': "7qxalsvy0e"}
+        }
+      };
+
+      let fetch_options = {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(event)
+      };
+
+      fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events?conferenceDataVersion=1', fetch_options)
+        .then((response) => response.json()) // Transform the data into json
+        .then(function(data) {
+          console.log(data);
+          chrome.storage.sync.set({meetID: data.hangoutLink}, function() {
+            console.log('Value is set to ' + data.hangoutLink);
+          });
+          chrome.storage.sync.set({email: data.creator.email}, function() {
+            console.log('Value is set to ' + data.creator.email);
+          });
+          chrome.runtime.sendMessage({
+            msg: "something_completed",
+            meetID: data.hangoutLink,
+            org: data.creator.email
+          });
+        })
+
+    });
+}
+
+chrome.runtime.onMessage
+  .addListener((request, sender, sendResponse) => {
+    if (request.message === 'get_event') {
+      createMeeting();
+    } else if(request.message === 'switch_user') {
+      chrome.identity
+        .getAuthToken({ interactive: true }, function (token) {
+          console.log(token);
+          if (!chrome.runtime.lastError) {
+
+            let url = 'https://accounts.google.com/o/oauth2/revoke?token=' + token;
+            fetch(url)
+              .then(() => {
+                console.log("after fetch")
+                chrome.identity.removeCachedAuthToken({token: token}, function (){
+                  console.log('removed');
+                  chrome.identity
+                    .getAuthToken({ interactive: true }, function (token) {
+                      console.log(token);
+                      chrome.runtime.sendMessage({
+                        msg: "user_changed"
+                      });
+                    });
+                });
+              });
+
+
+          }
+
+        });
+    }
+  });
+
---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import logo from '../../assets/img/logo.svg';
+import Greetings from '../../containers/Greetings/Greetings';
+import './Popup.css';
+import icon from '../../assets/img/meet.png';
+
+
+class Popup extends Component {
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      id:"",
+      email:"",
+      isLoading: false,
+    }
+  }
+
+  componentDidMount() {
+
+    chrome.storage.sync.get(['meetID'], (result) => {
+      console.log('Value currently is ' + result);
+      this.setState({
+        id: result.meetID
+      });
+    });
+    chrome.storage.sync.get(['email'], (result) => {
+      console.log('Value currently is ' + result);
+      this.setState({
+        email: result.email
+      });
+    });
+    chrome.runtime.onMessage.addListener(
+      (request, sender, sendResponse) => {
+        if (request.msg === "something_completed") {
+          //  To do something
+          console.log(request);
+
+          this.setState({
+            isLoading: false,
+            id: request.meetID,
+            email: request.org
+          });
+        }
+        if (request.msg === "copy_link") {
+          this.copyToClip();
+        }
+        if (request.msg === "user_changed") {
+          console.log("inside user change");
+          this.setState({
+            isLoading:false
+          });
+        }
+        if (request.msg === 'btn_press') {
+          console.log("inside btn press");
+          this.setState({
+            isLoading:true
+          });
+        }
+      }
+    );
+
+  }
+
+
+
+  switchUser = () => {
+    this.setState({
+      id:"",
+      email:"",
+      isLoading:true
+    });
+
+    chrome.storage.sync.set({meetID: ""}, function() {
+      console.log('Value is set to null');
+    });
+    chrome.storage.sync.set({email: ""}, function() {
+      console.log('Value is set to null');
+    });
+
+    chrome.runtime.sendMessage({message: 'switch_user'});
+  };
+
+  createMeet = () => {
+    this.setState({
+      isLoading: true
+    });
+    console.log("creating");
+    chrome.runtime.sendMessage({message: 'get_event'});
+  };
+
+
+  copyToClip = ()  =>  {
+    console.log("Copying...");
+    let content = document.getElementById("con");
+    content.select();
+    document.execCommand("copy");
+  };
+
+
+  render() {
+
+    return (
+      <div className="App">
+        <div className="nav">
+          <div className="welcome">
+
+          </div>
+          <div className="switch">
+            <h3 onClick={this.switchUser} className="switch-img">Switch User</h3>
+          </div>
+        </div>
+
+        <div className="main">
+          <div className="container">
+            <div className="meet">
+              <div className="icon"><img src={icon} width="40px" alt=""/></div>
+              <button className="meetBtn" onClick={this.createMeet} disabled={this.state.isLoading}>{this.state.isLoading?<div className="loader"></div>: <span>&#x2b; &nbsp;New meeting</span>}</button>
+            </div>
+            <div className="shortcut">
+              <p className="clipboard-para">Click above or press Alt+N to create a new meeting</p>
+            </div>
+          </div>
+          {this.state.id && <div className="clipboard">
+            <div>
+              <input className="clipboard-input" id="con" onClick={this.copyToClip} value={this.state.id} readOnly="readOnly" />
+              <p className="clipboard-para">Click above to copy the Link or press Alt+C</p>
+            </div>
+          </div>}
+        </div>
+        {this.state.email && <div>
+          <p className="footer">Organizer: {this.state.email}</p>
+        </div>}
+
+      </div>
+    );
+  }
+}
+
+export default Popup;
