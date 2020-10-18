0c76da864c90172957279d84b4c9a90c78b613ad
Anurag Sachdeva <anurag.sach28@gmail.com>
2020-10-18 17:37:12 +0530

    Documentation

--- README.md
+++ README.md
@@ -1,27 +1,5 @@
-# Chrome Extension Boilerplate with React 16.13 and Webpack 4
+### Chrome Extension for optimizing Google Meet creation
 
-[![npm](https://img.shields.io/npm/v/chrome-extension-boilerplate-react)](https://www.npmjs.com/package/chrome-extension-boilerplate-react)
-[![npm-download](https://img.shields.io/npm/dw/chrome-extension-boilerplate-react)](https://www.npmjs.com/package/chrome-extension-boilerplate-react)
-[![npm](https://img.shields.io/npm/dm/chrome-extension-boilerplate-react)](https://www.npmjs.com/package/chrome-extension-boilerplate-react)
-
-[![dependencies Status](https://david-dm.org/lxieyang/chrome-extension-boilerplate-react/status.svg)](https://david-dm.org/lxieyang/chrome-extension-boilerplate-react)
-[![devDependencies Status](https://david-dm.org/lxieyang/chrome-extension-boilerplate-react/dev-status.svg)](https://david-dm.org/lxieyang/chrome-extension-boilerplate-react?type=dev)
-
-## Features
-
-This is a basic Chrome Extensions boilerplate to help you write modular and modern Javascript code, load CSS easily and [automatic reload the browser on code changes](https://webpack.github.io/docs/webpack-dev-server.html#automatic-refresh).
-
-This boilerplate is updated with:
-
-- [React 16.13](https://reactjs.org)
-- [Webpack 4](https://webpack.js.org/)
-- [React Hot Loader](https://github.com/gaearon/react-hot-loader)
-- [eslint-config-react-app](https://www.npmjs.com/package/eslint-config-react-app)
-- [Prettier](https://prettier.io/)
-
-This boilerplate is heavily inspired by and adapted from [https://github.com/samuelsimoes/chrome-extension-webpack-boilerplate](https://github.com/samuelsimoes/chrome-extension-webpack-boilerplate), with additional support for React 16.13 features and Webpack 4.
-
-Please open up an issue to nudge me to keep the npm packages up-to-date. FYI, it takes time to make different packages with different versions work together nicely.
 
 ## Installing and Running
 
@@ -29,23 +7,52 @@ Please open up an issue to nudge me to keep the npm packages up-to-date. FYI, it
 
 1. Check if your [Node.js](https://nodejs.org/) version is >= **10.13**.
 2. Clone this repository.
-3. Change the package's `name`, `description`, and `repository` fields in `package.json`.
-4. Change the name of your extension on `src/manifest.json`.
-5. Run `npm install` to install the dependencies.
-6. Run `npm start`
-7. Load your extension on Chrome following:
+3. Run `npm install` to install the dependencies. If you get any authorization issues, try running `sudo npm install`
+4. Run `npm start` or `sudo npm start`
+5. Load your extension on Chrome following:
    1. Access `chrome://extensions/`
    2. Check `Developer mode`
    3. Click on `Load unpacked extension`
    4. Select the `build` folder.
-8. Happy hacking.
+6. Now you are all set to use the extension.
 
 ## Structure
 
-All your extension's code must be placed in the `src` folder.
+All the extension's code is placed in the `src` folder.
 
-The boilerplate is already prepared to have a popup, an options page, a background page, and a new tab page (which replaces the new tab page of your browser). But feel free to customize these.
+## Features
 
+This is a Chrome Extension to help you to ease the process of Google Meeting creation.
+Following are the basic features targeted  around this chrome extension:
+
+1. Allow users to create a new google meet meeting and and allow users to quickly share the link with others.
+2. Optimize meeting creation by allowing users to create and share links via keyboard alone.
+3. Allow users to choose which email to create a meeting with and cache the first selection for simultaneous meetings.
+
+### Keyboard Shortcuts
+1. `ALT+G` : Launch the Chrome Extension
+2. `ALT+N` : Create a new meeting
+3. `ALT+C` : Copy the meeting link onto Clipboard
+
+Combination of above keypress events are able to allow users to create and share the meeting via keyboard alone.
+
+This project is updated with:
+
+- [React 16.13](https://reactjs.org)
+- [Webpack 4](https://webpack.js.org/)
+- [React Hot Loader](https://github.com/gaearon/react-hot-loader)
+- [eslint-config-react-app](https://www.npmjs.com/package/eslint-config-react-app)
+- [Prettier](https://prettier.io/)
+
+
+
+Please open up an issue to nudge me to keep the npm packages up-to-date. FYI, it takes time to make different packages with different versions work together nicely.
+
+## Optimization Specification
+
+1. API_KEY and access codes to the google API are supposed to be hidden in a separate file and must not be shared. But for the sake of convenience and testing are shared with you.
+2. This project can be easily enhanced by adding a feature to open the newly created link in a new tab.
+3. New features like adding the attendees and notifying them can be very easily added as a feature because of the use of Google Calendar Api.
 ## Webpack auto-reload and HRM
 
 To make your workflow much more efficient this boilerplate uses the [webpack server](https://webpack.github.io/docs/webpack-dev-server.html) to development (started with `npm start`) with auto reload feature that reloads the browser automatically every time that you save some file in your editor.
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,135 +3,152 @@ import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 import icon from '../../assets/img/meet.png';
 
-
 class Popup extends Component {
-
   // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
     this.state = {
-      id:"",
-      email:"",
+      id: '',
+      email: '',
       isLoading: false,
-    }
+    };
   }
 
   componentDidMount() {
-
+    //fetch from local storage every time the popup is activated
     chrome.storage.sync.get(['meetID'], (result) => {
       console.log('Value currently is ' + result);
       this.setState({
-        id: result.meetID
+        id: result.meetID,
       });
     });
     chrome.storage.sync.get(['email'], (result) => {
       console.log('Value currently is ' + result);
       this.setState({
-        email: result.email
+        email: result.email,
       });
     });
-    chrome.runtime.onMessage.addListener(
-      (request, sender, sendResponse) => {
-        if (request.msg === "something_completed") {
-          //  To do something
-          console.log(request);
 
-          this.setState({
-            isLoading: false,
-            id: request.meetID,
-            email: request.org
-          });
-        }
-        if (request.msg === "copy_link") {
-          this.copyToClip();
-        }
-        if (request.msg === "user_changed") {
-          console.log("inside user change");
-          this.setState({
-            isLoading:false
-          });
-        }
-        if (request.msg === 'btn_press') {
-          console.log("inside btn press");
-          this.setState({
-            isLoading:true
-          });
-        }
+    //Listens to messages sent by background
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      if (request.msg === 'something_completed') {
+        //  To do something
+        console.log(request);
+
+        this.setState({
+          isLoading: false,
+          id: request.meetID,
+          email: request.org,
+        });
       }
-    );
-
+      if (request.msg === 'copy_link') {
+        this.copyToClip();
+      }
+      if (request.msg === 'user_changed') {
+        console.log('inside user change');
+        this.setState({
+          isLoading: false,
+        });
+      }
+      if (request.msg === 'btn_press') {
+        console.log('inside btn press');
+        this.setState({
+          isLoading: true,
+        });
+      }
+    });
   }
-
-
 
   switchUser = () => {
     this.setState({
-      id:"",
-      email:"",
-      isLoading:true
+      id: '',
+      email: '',
+      isLoading: true,
     });
 
-    chrome.storage.sync.set({meetID: ""}, function() {
+    chrome.storage.sync.set({ meetID: '' }, function () {
       console.log('Value is set to null');
     });
-    chrome.storage.sync.set({email: ""}, function() {
+    chrome.storage.sync.set({ email: '' }, function () {
       console.log('Value is set to null');
     });
 
-    chrome.runtime.sendMessage({message: 'switch_user'});
+    chrome.runtime.sendMessage({ message: 'switch_user' });
   };
 
   createMeet = () => {
     this.setState({
-      isLoading: true
+      isLoading: true,
     });
-    console.log("creating");
-    chrome.runtime.sendMessage({message: 'get_event'});
+    console.log('creating');
+    chrome.runtime.sendMessage({ message: 'get_event' });
   };
 
-
-  copyToClip = ()  =>  {
-    console.log("Copying...");
-    let content = document.getElementById("con");
+  copyToClip = () => {
+    console.log('Copying...');
+    let content = document.getElementById('con');
     content.select();
-    document.execCommand("copy");
+    document.execCommand('copy');
   };
-
 
   render() {
-
     return (
       <div className="App">
         <div className="nav">
-          <div className="welcome">
-
-          </div>
+          <div className="welcome"></div>
           <div className="switch">
-            <h3 onClick={this.switchUser} className="switch-img">Switch User</h3>
+            <h3 onClick={this.switchUser} className="switch-img">
+              Switch User
+            </h3>
           </div>
         </div>
 
         <div className="main">
           <div className="container">
             <div className="meet">
-              <div className="icon"><img src={icon} width="40px" alt=""/></div>
-              <button className="meetBtn" onClick={this.createMeet} disabled={this.state.isLoading}>{this.state.isLoading?<div className="loader"></div>: <span>&#x2b; &nbsp;New meeting</span>}</button>
+              <div className="icon">
+                <img src={icon} width="40px" alt="" />
+              </div>
+              <button
+                className="meetBtn"
+                onClick={this.createMeet}
+                disabled={this.state.isLoading}
+              >
+                {this.state.isLoading ? (
+                  <div className="loader"></div>
+                ) : (
+                  <span>&#x2b; &nbsp;New meeting</span>
+                )}
+              </button>
             </div>
             <div className="shortcut">
-              <p className="clipboard-para">Click above or press Alt+N to create a new meeting</p>
+              <p className="clipboard-para">
+                Click above or press Alt+N to create a new meeting
+              </p>
             </div>
           </div>
-          {this.state.id && <div className="clipboard">
-            <div>
-              <input className="clipboard-input" id="con" onClick={this.copyToClip} value={this.state.id} readOnly="readOnly" />
-              <p className="clipboard-para">Click above to copy the Link or press Alt+C</p>
+          {this.state.id && (
+            <div className="clipboard">
+              <div>
+                <input
+                  className="clipboard-input"
+                  id="con"
+                  onClick={this.copyToClip}
+                  value={this.state.id}
+                  readOnly="readOnly"
+                />
+                <p className="clipboard-para">
+                  Click above to copy the Link or press Alt+C
+                </p>
+              </div>
             </div>
-          </div>}
+          )}
         </div>
-        {this.state.email && <div>
-          <p className="footer">Organizer: {this.state.email}</p>
-        </div>}
-
+        {this.state.email && (
+          <div>
+            <p className="footer">Organizer: {this.state.email}</p>
+          </div>
+        )}
       </div>
     );
   }
